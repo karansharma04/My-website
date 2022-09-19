da3c08a014203a606d6236819e25c334781ad7fa
karan sharma <i-karan.sharma@devrev.ai>
2022-09-19 06:36:59 +0530

    .

--- src/Components/BodyComponents/Cp.js
+++ src/Components/BodyComponents/Cp.js
@@ -8,14 +8,16 @@ export default function Cp(props) {
             <div>
                 I give competitive programming contests in my free time. I'm a <span className="text-zinc-200">5 star rated coder</span> on CodeChef and <span className="text-zinc-200">Expert</span> on Codeforces.
             </div>
-            <div className="flex gap-4 leading-8">
+            <div className="flex flex-col justify-center items-center sm:flex-row sm:justify-start  gap-4 leading-8">
                 <div className="align-baseline">My Handles:</div>
+                <div className="flex justify-center">
                 <div><a href="https://codeforces.com/profile/" rel="noopener noreferrer" target="_blank">
                     <SiCodeforces className="transition-all ease-linear hover:scale-125 hover:text-yellow-100 text-white hover:-translate-y-1" size="2em" />
                 </a></div>
                 <div className="px-4"><a href="https://www.codechef.com/users/karan_04" rel="noopener noreferrer" target="_blank">
                     <SiCodechef className="transition-all ease-linear hover:scale-125 hover:text-yellow-100 text-white hover:-translate-y-1" size="2em"  />
                 </a></div>
+                </div>
             </div>
         </div>
     )
