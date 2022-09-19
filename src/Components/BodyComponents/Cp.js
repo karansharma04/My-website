a89d50d74c8dfc2003dfd576487b2ad45e9996a2
karan sharma <i-karan.sharma@devrev.ai>
2022-09-19 19:05:27 +0530

    minor change

--- src/Components/BodyComponents/Cp.js
+++ src/Components/BodyComponents/Cp.js
@@ -11,7 +11,7 @@ export default function Cp(props) {
             <div className="flex flex-col justify-center items-center sm:flex-row sm:justify-start  gap-4 leading-8">
                 <div className="align-baseline">My Handles:</div>
                 <div className="flex justify-center">
-                <div><a href="https://codeforces.com/profile/" rel="noopener noreferrer" target="_blank">
+                <div><a href="https://codeforces.com/profile/_karan" rel="noopener noreferrer" target="_blank">
                     <SiCodeforces className="transition-all ease-linear hover:scale-125 hover:text-yellow-100 text-white hover:-translate-y-1" size="2em" />
                 </a></div>
                 <div className="px-4"><a href="https://www.codechef.com/users/karan_04" rel="noopener noreferrer" target="_blank">
