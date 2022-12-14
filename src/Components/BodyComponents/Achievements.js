3362ddb36445dca04946fb58a63b2fc554d9115b
karansharma04 <kkaarraann01@gmail.com>
2022-12-14 21:08:00 +0530

    .

--- src/Components/BodyComponents/Achievements.js
+++ src/Components/BodyComponents/Achievements.js
@@ -1,34 +1,57 @@
-import Title from "../Title"
-
+import Title from "../Title";
+import { useState } from "react";
 
 export default function Achievements(props) {
-    return (
-        <div ref={props.achievementsRef} className="flex flex-col gap-10 pb-8 sm:pb-16 md:pb-24 lg:pb-32">
-            <Title>achievements</Title>
-            <div className="flex flex-col sm:flex-row">
-                <div className="text-color-hover px-2">jeeAdvanced2019:</div>
-                <div>Secured All India Rank of 510 among 1.6 million contesting candidates, one of the most prestigious competitive exams of India.</div>
-            </div>
-            <div className="flex flex-col sm:flex-row">
-                <div className="text-color-hover px-2">jeeMains2019:</div>
-                <div>Secured 99.76 percentile among ~10 million contesting candidates.</div>
-            </div>
-            <div className="flex flex-col sm:flex-row">
-                <div className="text-color-hover px-2">codeforcesDiv2:</div>
-                <div>Got a global rank of 470 particicpating in Codeforces Div 2 round among ~30000 particicpants.</div>
-            </div>
-            <div className="flex flex-col sm:flex-row">
-                <div className="text-color-hover px-2">codeChef:</div>
-                <div>Got a global rank of 70 in CodeChef Lunchtime.</div>
-            </div>
-            <div className="flex flex-col sm:flex-row">
-                <div className="text-color-hover px-2">googleKickstart2022:</div>
-                <div>Got a global rank of 1466 in Round G of Google Kickstart 2022 among ~13000 particicpants.</div>
-            </div>
-            <div className="flex flex-col sm:flex-row">
-                <div className="text-color-hover px-2">googleCodeJam2022:</div>
-                <div>Got a global rank of 1855 in Round 1 of Google Code Jam 2022 among ~13000 particicpants.</div>
-            </div>
+  const achievements = [
+    {
+      title: "jeeAdvanced2019",
+      body: "Secured All India Rank of 510 among 1.6 million contesting candidates, one of the most prestigious competitive exams of India.",
+    },
+    {
+      title: "jeeMains2019",
+      body: "Secured 99.76 percentile among ~10 million contesting candidates.",
+    },
+    {
+      title: "codeChef",
+      body: "Max rating of 2132(5-star) on CodeChef.",
+    },
+    {
+      title: "codeforces",
+      body: "Max rating of 1707(expert) on Codeforces.",
+    },
+    {
+      title: "codeChef",
+      body: "Got a global rank of 70 in CodeChef Lunchtime.",
+    },
+    {
+      title: "codeforcesDiv2",
+      body: "Got a global rank of 470 particicpating in Codeforces Div 2 round among ~30000 particicpants.",
+    },
+    {
+      title: "googleKickstart2022",
+      body: "Got a global rank of 1466 in Round G of Google Kickstart 2022 among ~13000 particicpants.",
+    },
+    {
+      title: "googleCodeJam2022",
+      body: "Got a global rank of 1855 in Round 1 of Google Code Jam 2022 among ~13000 particicpants.",
+    },
+  ];
+
+  return (
+    <div
+      ref={props.achievementsRef}
+      className="flex flex-col gap-10 pb-8 sm:pb-16 md:pb-24 lg:pb-32"
+    >
+      <Title>achievements</Title>
+
+      {achievements.map((achievement) => (
+        <div key={achievement.title} className="flex flex-col sm:flex-row">
+          <div className={`text-color-hover px-2`}>
+            {achievement.title + ":"}
+          </div>
+          <div>{achievement.body}</div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
